3b92dd8fc5bc3ddf997fced4aa33c38bb407afed
existdissolve <existdissolve@gmail.com>
2013-05-19 13:27:16 -0500

    Part One

---
+++ PartOne/cartracker/app/controller/Main.js
@@ -0,0 +1,3 @@
+Ext.define('CarTracker.controller.Main', {
+    extend: 'Ext.app.Controller'
+});
---
+++ PartOne/cartracker/app/view/Main.js
@@ -0,0 +1,4 @@
+Ext.define("CarTracker.view.Main", {
+    extend: 'Ext.Component',
+    html: 'Hello, World!!'
+});
